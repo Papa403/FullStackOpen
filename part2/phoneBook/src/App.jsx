15ad87a5981e753d0bae8cd7b8d660bc03b35ab2
Papa403 <alilykai@gmail.com>
2025-08-01 21:50:26 -0500

    exercise2_9: implement a search feature (case insensitive)

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber] = useState('')
+  const [search,setNewSearch] = useState('')
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
@@ -18,16 +22,29 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({name: newName, number: newNumber}))
+    const nextId = persons.length > 0 ? Math.max(...persons.map(p => p.id)) + 1 : 1
+    setPersons(persons.concat({name: newName, number: newNumber,id:nextId}))
     setNewName('')
     setNewNumber('')
   }
   const handleNewNumber = (event) => {
     setNewNumber(event.target.value)
   }
+  const handleNewSearch = (event) => {
+    setNewSearch(event.target.value)
+  }
+  const personsToShow = search
+    ? persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+    : persons
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        filter shown with <input value={search} onChange={handleNewSearch}/>
+      </form>
+      <h2>
+        add a new
+      </h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNewName}/>
@@ -40,7 +57,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person=><div>{person.name} {person.number}</div>)}
+      {personsToShow.map(person=><div>{person.name} {person.number}</div>)}
     </div>
   )
 }
