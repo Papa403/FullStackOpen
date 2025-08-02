0c777e75cadd7c8ffede03e9894d0863fd68a168
Papa403 <alilykai@gmail.com>
2025-08-01 19:09:09 -0500

    exercise2_8: number can now also be added along with name

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     { name: 'Arto Hellas'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber,setNewNumber] = useState('')
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
@@ -17,8 +18,12 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
       return
     }
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({name: newName, number: newNumber}))
     setNewName('')
+    setNewNumber('')
+  }
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value)
   }
   return (
     <div>
@@ -28,11 +33,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNewName}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNewNumber}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person=><div>{person.name}</div>)}
+      {persons.map(person=><div>{person.name} {person.number}</div>)}
     </div>
   )
 }
