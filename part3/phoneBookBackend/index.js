1e05f150a638e6d093e5d93f1b8e403fb98835b9
Papa403 <alilykai@gmail.com>
2025-08-08 17:04:36 -0500

    exercise3_3: implemented ability to get single phonebook entry, if it doesn't exist get error 404

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -39,6 +39,15 @@ app.get('/info', (request, response) => {
     <p>${new Date()}</p>`)
 })
 
+app.get('/api/persons/:id',(request, response)=> {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+  if(!person) {
+    return response.status(404).end()
+  }
+  response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
