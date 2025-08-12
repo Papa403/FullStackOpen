deb5397f41f6e03c4dad55ab51124c36b1335757
Papa403 <alilykai@gmail.com>
2025-08-12 18:45:36 -0500

    uibuild

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -40,6 +40,7 @@ morgan.token('body', (request, response) => {
 })
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 const generateId = () => {
