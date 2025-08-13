54b8aec9faaa7a9e1e6c1cc275ab7bca6e11b4ed
Papa403 <alilykai@gmail.com>
2025-08-13 15:55:09 -0500

    exercise3_12: installed mongoose and connected cloud instance of mongoDB. Code can add to db and retrieve from db based on args provided

---
+++ part3/phoneBookBackend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 2) {
+  console.log('give pw as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://alilykai:${password}@cluster0.hkb9zch.mongodb.net/phoneBookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const phoneBookSchema = new mongoose.Schema({
+  id: String,
+  name: String,
+  number: String,
+})
+
+const Entry = mongoose.model('Entry', phoneBookSchema)
+
+const entry = new Entry({
+  id:3,
+  name:process.argv[3],
+  number:process.argv[4],
+})
+
+if (process.argv.length === 5) {
+  entry.save().then(result => {
+  console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+  mongoose.connection.close()
+})
+}
+
+if (process.argv.length === 3) {
+  Entry.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(entry => 
+      console.log(`${entry.name} ${entry.number}`))
+    mongoose.connection.close()
+  })
+}
