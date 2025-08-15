71d0657785c7b72c9323fbef5777acc32b6a50d7
Papa403 <alilykai@gmail.com>
2025-08-15 12:46:09 -0500

    exercise3_22: removed requestLogger (redundant because of morgan) and fixed linter issues

--- part3/phoneBookBackend/index.js
+++ part3/phoneBookBackend/index.js
@@ -4,15 +4,7 @@ const morgan = require('morgan')
 const Entry = require('./models/person')
 const app = express()
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-morgan.token('body', (request, response) => {
+morgan.token('body', (request) => {
   return request.method === 'POST'
     ? JSON.stringify(request.body)
     : ' '
@@ -20,7 +12,7 @@ morgan.token('body', (request, response) => {
 
 app.use(express.json())
 app.use(express.static('dist'))
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/', (request,response) => {
   response.send('<h1>Hello person<h1>')
@@ -30,7 +22,7 @@ app.get('/api/persons', (request,response) => {
   Entry.find({})
     .then(person => {
       response.json(person)
-  })
+    })
 })
 
 app.get('/info', (request, response, next) => {
@@ -42,16 +34,16 @@ app.get('/info', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.get('/api/persons/:id',(request, response)=> {
+app.get('/api/persons/:id',(request, response) => {
   Entry.findById(request.params.id)
     .then(person => {
       response.json(person)
-  })
+    })
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Entry.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -61,23 +53,22 @@ app.post('/api/persons', (request,response, next) => {
   const body = request.body
 
   if(!body.name || !body.number) {
-    return response.status(400).json({error: 'content missing'})
+    return response.status(400).json({ error: 'content missing' })
   }
 
   const entry = new Entry({
     name: body.name,
     number: body.number,
   })
-  
   entry.save()
     .then(savedPerson => {
-    response.json(savedPerson)
-  })
+      response.json(savedPerson)
+    })
     .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response) => {
-  const {name, number} = request.body
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
   Entry.findById(request.params.id)
     .then((entry) => {
@@ -93,19 +84,19 @@ app.put('/api/persons/:id', (request, response) => {
       })
     })
     .catch((error) => next(error))
-}) 
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({error: 'unknown endpoint'})
-} 
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
-  
+
   if(error.name === 'ValidationError') {
-    return response.status(400).send({error: `${error.message}`})
+    return response.status(400).send({ error: `${error.message}` })
   }
   next(error)
 }
--- part3/phoneBookBackend/mongo.js
+++ part3/phoneBookBackend/mongo.js
@@ -26,17 +26,16 @@ const entry = new Entry({
 })
 
 if (process.argv.length === 5) {
-  entry.save().then(result => {
-  console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
-  mongoose.connection.close()
-})
+  entry.save().then(() => {
+    console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
 if (process.argv.length === 3) {
   Entry.find({}).then(result => {
     console.log('phonebook:')
-    result.forEach(entry => 
-      console.log(`${entry.name} ${entry.number}`))
+    result.forEach(entry => console.log(`${entry.name} ${entry.number}`))
     mongoose.connection.close()
   })
 }
