5ee98c361f728e0ef0d6b5e9b2969873d4c13a6b
Papa403 <alilykai@gmail.com>
2025-08-15 21:00:11 -0500

    refactored blogSchema into its own file

--- part4/blogListBackend/app.js
+++ part4/blogListBackend/app.js
@@ -4,14 +4,9 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+
+
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
