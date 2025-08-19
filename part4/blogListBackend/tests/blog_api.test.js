775750ea4e28508fa6ee04511c8954379792b5ef
Papa403 <alilykai@gmail.com>
2025-08-18 23:56:43 -0500

    exercise4_10: tests POST request makes a new blog

--- part4/blogListBackend/tests/blog_api.test.js
+++ part4/blogListBackend/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -6,6 +6,12 @@ const app = require('../app')
 const helper = require('./test_helper')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  const Blog = require('../models/blog')
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
 
 test('correct amount of blog posts returned', async () => {
   const response = await api
@@ -19,6 +25,29 @@ test('unique identifier property of blog posts is named id', async () => {
   const response = await api.get('/api/blogs')
   const blog = response.body[0]
   assert(blog.id)
+})
+
+test('successfully created a new blog', async () => {
+  const newNote = {
+    title:'test note added',
+    author:'me in the code',
+    url:'http://localhost.com',
+    likes:'42'
+  }
+  await api
+    .post('/api/blogs')
+    .send(newNote)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+  
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+  assert(blogsAtEnd.some(blog =>
+    blog.title === newNote.title &&
+    blog.author === newNote.author &&
+    blog.url === newNote.url &&
+    blog.likes == newNote.likes
+  ))
 })
 
 after(async () => {
