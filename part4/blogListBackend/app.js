30f90307f620fed6d32190c547b5933a8ba7ef46
Papa403 <alilykai@gmail.com>
2025-08-20 15:19:11 -0500

    created usersRouter and way to post to it

--- part4/blogListBackend/app.js
+++ part4/blogListBackend/app.js
@@ -4,6 +4,7 @@ const { info, error } = require('./utils/logger')
 const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 const app = express()
 
@@ -22,6 +23,7 @@ mongoose
 
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 
 
