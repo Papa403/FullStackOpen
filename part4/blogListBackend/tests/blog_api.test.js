dd9afee9d402e80e83cf640822e0205c8081e43a
Papa403 <alilykai@gmail.com>
2025-08-20 19:37:28 -0500

    add user creation tests and setup in blog_api.test.js

--- part4/blogListBackend/tests/blog_api.test.js
+++ part4/blogListBackend/tests/blog_api.test.js
@@ -1,9 +1,11 @@
-const { test, after, beforeEach } = require('node:test')
+const bcrypt = require('bcrypt')
+const { test, after, describe, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const helper = require('./test_helper')
+const User = require('../models/user')
 
 const api = supertest(app)
 
@@ -106,6 +108,38 @@ test('updating likes of a resource', async () => {
   assert.strictEqual(updatedBlog.likes, updatedLikes)
 })
 
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'papa403',
+      name: 'Papa 403',
+      password: 'bloglistTestPW',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+})
 
 after(async () => {
   await mongoose.connection.close()
