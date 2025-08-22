9e5c308f5be89780079e8b9d97419b7274e2cc9b
Papa403 <alilykai@gmail.com>
2025-08-22 10:34:42 -0500

    exercise4_18: add login functionality with JWT authentication

--- part4/blogListBackend/app.js
+++ part4/blogListBackend/app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -24,7 +25,7 @@ mongoose
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
-
+app.use('/api/login', loginRouter)
 
 
 
